508e73b545d47a0bb0184d9e5f573bd9572a4b2b
khudoyor01 <nasima.sharipova1967@gmail.com>
2021-08-05 13:30:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// let son1 = prompt("sum kiriting");
+// let son1 = 45.0155654;
+
+// son1 = Math.round(son1 * 100) / 100;
+// console.log(son1);
+
+// son1 = Math.round(son1);
+// console.log(son1);
+
+// let d = son1 / 10654;
+// let e = son1 / 12674;
+
+// if (e < 1 || d < 1) {
+//   alert(`Do'llar kursi ${e}
+// EURO kursi ${d}`);
+// } else if (e >= 1 || d >= 1) {
+//   alert(`Do'llar kursi ${Math.ceil(e)}
+// EURO ${Math.ceil(d)}`);
+// }
+
+// let date = new Date();
+
+// console.log(date.getDate());
+
+// Function
+// Logical
+// Ternary
+// If else
+
+// Function
+
+let name = prompt("ism");
+
+function greeting(him) {
+  alert(`salom ${him}`);
+}
+
+greeting(name);
